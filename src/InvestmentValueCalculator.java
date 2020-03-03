d36ca470aa437d769381adb58ce46e61c4ea849f
atunsal <alpertuna.unsal@ontariotechu.net>
2020-03-03 11:05:49 -0500

    Questions 2 and 4

---
+++ src/InvestmentValueCalculator.java
@@ -0,0 +1,105 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class InvestmentValueCalculator extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        LoanInvestCalculator calcPane = new LoanInvestCalculator();
+
+        primaryStage.setScene(new Scene(calcPane, calcPane.getPrefWidth(), calcPane.getPrefHeight()));
+        primaryStage.setTitle("Investment-Value Calculator");//Title of the calculator
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+        Application.launch(args);//launch the calculator
+    }
+
+
+    private class LoanInvestCalculator extends GridPane {
+
+        //label of the first text field
+        Label amountLabel = new Label("Investment Amount");
+        TextField amountText = new TextField();
+
+        //label of the second text field
+        Label yearsLabel = new Label("Years");
+        TextField yearsText = new TextField();
+
+        //label of the third text field
+        Label rateLabel = new Label("Annual Interest Rate");
+        TextField rateText = new TextField();
+
+        //label of the fourth text field
+        Label futureValueLabel = new Label("Future Value");
+        TextField futureValueText = new TextField();
+
+        //setting up a calculate button
+        Button btCalc = new Button("Calculate");
+
+        private LoanInvestCalculator() {
+
+            //create a calculator layout
+            setPadding(new Insets(10, 10, 10, 10));
+            setAlignment(Pos.CENTER);
+            setHgap(10);
+            setVgap(10);
+
+            add(amountLabel, 0, 0);
+            add(amountText, 1, 0);
+
+            add(yearsLabel, 0, 1);
+            add(yearsText, 1, 1);
+
+            add(rateLabel, 0, 2);
+            add(rateText, 1, 2);
+
+            add(futureValueLabel, 0, 3);
+            add(futureValueText, 1, 3);
+
+            HBox buttons = new HBox();
+            buttons.getChildren().add(btCalc);
+            buttons.setAlignment(Pos.BOTTOM_RIGHT);//button position
+            add(buttons, 1, 4);
+            btCalc.setOnAction(e-> calcFutureValue());
+
+            // Editing TextField settings
+            TextField[] textFields = (TextField[])getArray(
+                    amountText, yearsText, rateText, futureValueText);//array of textfields
+
+            for (TextField tf : textFields) {
+                tf.setAlignment(Pos.BASELINE_RIGHT);//set alignment of text fields to right
+            }
+            futureValueText.setDisable(true);//so that we cannot write in future value text field
+
+        }
+
+        private Object[] getArray(Object... objects) {
+            Object[] temp = new TextField[objects.length];
+            for (int i = 0; i < objects.length; i++) {
+                temp[i] = objects[i];
+            }
+            return temp;
+        }
+
+        public void calcFutureValue() {
+            double investmentAmount = Double.parseDouble(amountText.getText());
+            double years = Double.parseDouble(yearsText.getText());
+            double monthInterestRate = Double.parseDouble(rateText.getText()) / 12 / 100;
+
+            double futureValue = investmentAmount * Math.pow(1 + monthInterestRate, years * 12);
+            futureValueText.setText(String.format("$%.2f", futureValue));//set text to calculated value
+        }
+    }
+}
